230c22823bee6ed91b73a5b83f83fde81f3bf97a
CHL-a <eacclickbait@gmail.com>
2022-08-31 21:16:25 -0700

    2.2

--- src_V1/main.lua
+++ src_V1/main.lua
@@ -59,8 +59,34 @@ end).onInvalidRequest(function (client, req, res)
 	)
 
 	print(req.webPage,
-		Static.table.toString(webPage)
-)
+		Static.table.toString(webPage))
+
+	if not (webPage[2] == 'robloxproxy' and webPage[3] == 'v2') then
+		return
+	end
+
+	local authKey = req.headers['Proxy-Auth-Key']
+
+	if not (not authKey or authKey == Environment.get 'authKey') then
+		res.statusCode = 401
+		res.body = 'Bad Request: Unauthorized'
+		return
+	end
+
+	if req.requestType ~= 'GET' then
+		res.statusCode = 400
+		req.body = 'Bad Request: Expected GET Request'
+		return
+	end
+
+	local url = ('https://%s.roblox.com'):format(
+		webPage[4],
+		table.concat(webPage, '/', 5)
+	)
+
+	print(url)
+
+	-- local resA = cURL.get(url)
 
 
 end).launch()
